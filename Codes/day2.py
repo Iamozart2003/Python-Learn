785ea9fcf0854cb3fbaac6d4673d19147bf6a291
Iamozart2003 <125468003+Iamozart2003@users.noreply.github.com>
2023-02-25 10:21:07 +0530

    First_commit

---
+++ Codes/day2.py
@@ -0,0 +1,78 @@
+# type of 
+
+# x = 1E4
+# y = 1j
+# z = -35.59
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+#-----random NUmber-----
+# import random
+# print(random.randrange(1, 10))
+
+# -----string------
+# a = "hello World"
+# print(a[6])
+
+# -----for loop-----
+# for x in "banana":
+#   print(x)
+
+# Python program to illustrate
+# Iterating over a list
+# print("List Iteration")
+# l = ["geeks", "for", "geeks"]
+# for i in l:
+#     print(i)
+ 
+# # Iterating over a tuple (immutable)
+# print("\nTuple Iteration")
+# t = ("geeks", "for", "geeks")
+# for i in t:
+#     print(i)
+ 
+# # Iterating over a String
+# print("\nString Iteration")
+# s = "Geeks"
+# for i in s:
+#     print(i)
+ 
+# # Iterating over dictionary
+# print("\nDictionary Iteration")
+# d = dict()
+# d['xyz'] = 123
+# d['abc'] = 345
+# for i in d:
+#     print("%s  %d" % (i, d[i]))
+ 
+# # Iterating over a set
+# print("\nSet Iteration")
+# set1 = {1, 2, 3, 4, 5, 6}
+# for i in set1:
+#     print(i),
+
+# -----in -----
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# txt = "The best things in life are "
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+# else:
+#     print("fuck off")  
+
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i,end='')
+#     print()
+
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
