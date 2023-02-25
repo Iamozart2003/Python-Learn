a7bd69545537109e4b8664181f08cd5bbd666c20
Premsengar10 <premsengar28@gmail.com>
2023-02-25 22:50:39 +0530

    die

--- Codes/day3.py
+++ Codes/day3.py
@@ -1,5 +1,38 @@
-x = "Hello"
-y = 15
+# x = "Hello"
+# y = 15
 
-print(bool(x))
-print(bool(y))
+# print(bool(x))
+# print(bool(y))
+
+# def myFunction() :
+#   return True
+
+# print(myFunction()) 
+
+
+# -----isinstance----
+# x = 200
+# print(isinstance(x, int))
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# def my_function(country = "Norway"):
+#   print("I am from " + country)
+
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Brazil")
+
+# def inter(x):
+#     return 7 * x
+
+# print(inter(5))
